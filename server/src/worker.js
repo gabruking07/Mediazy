9b8351074ae58d865a5060e5d435e5524e17db1d
Raj Patel <rajpatel603603@gmail.com>
2026-06-25 14:14:25 +0530

    clear

--- server/src/worker.js
+++
@@ -1,9 +0,0 @@
-import { connectDatabase } from './config/db.js';
-import { ensureDownloadDir } from './utils/files.js';
-import { startDownloadWorker } from './workers/downloadWorker.js';
-
-await ensureDownloadDir();
-await connectDatabase();
-startDownloadWorker();
-
-console.log('Mediazy download worker running');
